53d75b47a5e9c18107aedcd2674fd04743a30383
Hypietos <duobedwars@gmail.com>
2022-07-02 17:17:09 -0500

    add source for future projects

--- src/modules/util.js
+++ src/modules/util.js
@@ -1,4 +1,4 @@
-exports.getWoolWarsLevel = async (exp) => {
+exports.getWoolWarsLevel = async (exp) => { //https://hypixel.net/threads/discord-js-get-woolwars-star-from-exp.4949443/post-35669607
   const minimalExp = [0, 1e3, 3e3, 6e3, 1e4, 15e3]; // NB: progression is 1k, 2k, 3k, 4k, 5k
   const baseLevel = minimalExp.length;
   const baseExp = minimalExp[minimalExp.length - 1];
