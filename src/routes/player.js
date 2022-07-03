923c41a3c3aa3a6a513cbe55937ab6f474b244c4
Armic <71387232+imarmic@users.noreply.github.com>
2022-07-03 04:56:24 +0200

    fixed formatting and fixed the url being wrong

--- src/routes/player.js
+++ src/routes/player.js
@@ -14,20 +14,21 @@ module.exports = {
   async run(req, res) {
     if (!req.query || !req.query.key || !req.query.uuid) return res.status(400).send({ success: false, error: 'Missing query parameters' });
 
-    let response = await axios.get(`https://api.hypixel.net/key?key=${req.query.key}`).catch(err => {
+    let response = await axios.get(`https://api.hypixel.net/player?key=${req.query.key}&uuid=${req.query.uuid}`).catch(err => {
       console.log(err)
       return res.status(err?.response?.status || 500).send({ success: false, error: err })
     });
 
     if (res.headersSent) return; //if the response has already been sent, don't continue
-
+  
     res.status(200).send({
       success: true,
       player: {
         uuid: req.query.uuid,
-        rank: response.data.player.rank, firstLogin:
-        response.data.player.firstLogin, lastLogin:
-        response.data.player.lastLogin,
+        username: response.data.player.displayname,
+        rank: response.data.player.rank, 
+        firstLogin: response.data.player.firstLogin,
+        lastLogin: response.data.player.lastLogin,
         stats: await getStats(response)
       }
     })
